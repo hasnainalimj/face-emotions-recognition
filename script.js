94e5c256db37189a1bea0578b71f8f0de0c9312a
hasnainalimj <mjhasnain15@gmail.com>
2019-10-13 03:07:08 +0500

    fixed issue camera access

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const video = document.getElementById("video");
 
+//Loading Models of Face API
+Promise.all([
+  faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
+  faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+  faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+  faceapi.nets.faceExpressionNet.loadFromUri("/models")
+]).then(startVideo);
+
+//Face Emotions Recognition
 function startVideo() {
   navigator.getUserMedia(
     { video: {} },
@@ -7,5 +16,3 @@ function startVideo() {
     error => console.log("error while starting video", error)
   );
 }
-
-startVideo();
